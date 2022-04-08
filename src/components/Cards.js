1defe911d5950309100bd1f22bd3fc8e32c7c615
tysepa <tysepa88@gmail.com>
2022-04-08 22:37:21 +0200

    updade web react

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,34 +10,34 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              src='images/img-9.jpeg'
+              text='Explore the hidden waterfall deep inside the Rwanda Park'
               label='Adventure'
               path='/services'
             />
             <CardItem
-              src='images/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
+              src='images/img-2.jpeg'
+              text='Gallery of Womens Opportunity Center in Rwanda '
               label='Luxury'
               path='/services'
             />
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              src='images/img-3.jpeg'
+              text='Kigali Full Day City Tour| Join Up Safaris Ikaze Iwacu Mu Rwanda'
               label='Mystery'
               path='/services'
             />
             <CardItem
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
+              src='images/img-4.jpeg'
+              text='Hotel and Accommodation Booking at 100s of Thousands Destinations Worldwide. Get the Best Rate for Your Next Hotel Stay'
               label='Adventure'
               path='/products'
             />
             <CardItem
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
+              src='images/img-1.jpeg'
+              text='The city features a resort on the shores of Lake Kivu, with several hotels and three sandy beaches'
               label='Adrenaline'
               path='/sign-up'
             />
